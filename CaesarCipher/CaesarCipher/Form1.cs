f17ed638a6ff27de3adbb145a272c20d04de1214
Konjuhi <ardit.konjuhi@hotmail.com>
2019-05-06 02:54:59 +0200

    Enkriptimi dhe dekriptimi

--- CaesarCipher/CaesarCipher/Form1.cs
+++ CaesarCipher/CaesarCipher/Form1.cs
@@ -8,13 +8,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CaesarCipher
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace CaesarCipher {
+    public partial class Form1: Form {
+        public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            textBox1.Text = "Hello";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                textBox2.Text = Caesar.encrypt(textBox1.Text, Convert.ToInt32(textBox3.Text));
+            } catch(Exception ex) {
+                MessageBox.Show("Ju lutemi mbushini te gjithe hapesirat per tekst.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            try {
+                MessageBox.Show(Caesar.decrypt(textBox2.Text, Convert.ToInt32(textBox4.Text)));
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Ju lutemi mbushini te gjithe hapesirat per tekst.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+    }
+
+    static class Caesar {
+        public static string encrypt(string str, int shift) {
+            string encryptedString = "";
+            for (int i = 0; i < str.Length; i++) {
+                encryptedString += (char)((int)str[i]+shift);
+            }
+            return encryptedString;
+        }
+
+        public static string decrypt(string str, int shift) {
+            string decryptedString = "";
+            for (int i = 0; i < str.Length; i++)
+                decryptedString += (char)((int)str[i]-shift);
+            return decryptedString;
         }
     }
 }
--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -4,16 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CaesarCipher
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+namespace CaesarCipher {
+    static class Program {
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
