8756de53e0a0d6e55406128439c6760a95eb7de5
rrezoni <rezon.hazrolli@hotmail.com>
2019-05-07 00:18:06 +0200

    Permiresime

--- CaesarCipher/CaesarCipher/Form1.Designer.cs
+++ CaesarCipher/CaesarCipher/Form1.Designer.cs
@@ -40,28 +40,35 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Location = new System.Drawing.Point(597, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 30);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 2;
             this.button1.Text = "Encrypt";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Location = new System.Drawing.Point(597, 23);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 30);
-            this.button2.TabIndex = 1;
+            this.button2.TabIndex = 8;
             this.button2.Text = "Decrypt";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
@@ -78,7 +85,7 @@
             this.textBox1.Location = new System.Drawing.Point(100, 27);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(336, 22);
-            this.textBox1.TabIndex = 3;
+            this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
@@ -95,13 +102,13 @@
             this.textBox2.Location = new System.Drawing.Point(100, 27);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(336, 22);
-            this.textBox2.TabIndex = 5;
+            this.textBox2.TabIndex = 6;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(293, 398);
+            this.progressBar1.Location = new System.Drawing.Point(276, 398);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(116, 23);
+            this.progressBar1.Size = new System.Drawing.Size(150, 23);
             this.progressBar1.TabIndex = 6;
             // 
             // textBox3
@@ -109,7 +116,7 @@
             this.textBox3.Location = new System.Drawing.Point(488, 27);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(103, 22);
-            this.textBox3.TabIndex = 7;
+            this.textBox3.TabIndex = 1;
             // 
             // label3
             // 
@@ -125,7 +132,7 @@
             this.textBox4.Location = new System.Drawing.Point(488, 27);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(103, 22);
-            this.textBox4.TabIndex = 9;
+            this.textBox4.TabIndex = 7;
             // 
             // label4
             // 
@@ -139,6 +146,8 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.textBox5);
             this.groupBox1.Controls.Add(this.textBox1);
@@ -164,15 +173,18 @@
             // 
             // textBox5
             // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Control;
             this.textBox5.Location = new System.Drawing.Point(100, 59);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(572, 22);
-            this.textBox5.TabIndex = 9;
+            this.textBox5.TabIndex = 3;
             // 
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button4);
             this.groupBox2.Controls.Add(this.textBox6);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label2);
@@ -193,7 +205,7 @@
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(572, 22);
-            this.textBox6.TabIndex = 12;
+            this.textBox6.TabIndex = 9;
             // 
             // label6
             // 
@@ -204,12 +216,67 @@
             this.label6.TabIndex = 11;
             this.label6.Text = "Plain Text: ";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(342, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Send Data";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(342, 120);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Send Data";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(606, 404);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 13;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(236, 120);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button6.Location = new System.Drawing.Point(236, 120);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 34);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Clear";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(702, 433);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.progressBar1);
@@ -224,6 +291,7 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -246,6 +314,11 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 
 }
--- CaesarCipher/CaesarCipher/Form1.cs
+++ CaesarCipher/CaesarCipher/Form1.cs
@@ -20,27 +20,62 @@ namespace CaesarCipher {
 
         private void button1_Click(object sender, EventArgs e) {
             try {
-                if(Convert.ToInt32(textBox3.Text) < 0)
-                    throw new Exception();
+                //if(Convert.ToInt32(textBox3.Text) < 0)
+                //    throw new Exception();
                 textBox5.Text = Caesar.encrypt(textBox1.Text, Convert.ToInt32(textBox3.Text));
             } catch(Exception ex) {
-                MessageBox.Show("Ju lutemi mbushini te gjitha kutite.\nQelesi duhet te jete nje numer i plote.");
+                MessageBox.Show("Ju lutemi mbushini te gjitha kutite e enkriptimit.\nQelesi duhet te jete nje numer pozitiv i plote.");
             }
         }
 
         private void button2_Click(object sender, EventArgs e) {
             try {
-                if(Convert.ToInt32(textBox4.Text) < 0)
-                    throw new Exception();
+                //if(Convert.ToInt32(textBox4.Text) < 0)
+                //    throw new Exception();
                 textBox6.Text = Caesar.decrypt(textBox2.Text, Convert.ToInt32(textBox4.Text));
             }
             catch(Exception ex) {
-                MessageBox.Show("Ju lutemi mbushini te gjitha kutite.\nQelesi duhet te jete nje numer i plote.");
+                MessageBox.Show("Ju lutemi mbushini te gjitha kutite e dekriptimit.\nQelesi duhet te jete nje numer pozitiv i plote.");
             }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e) {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            try {
+                if(textBox5.Text == null || textBox3.Text == null)
+                    throw new Exception();
+                textBox2.Text = textBox5.Text;
+                textBox4.Text = textBox3.Text;
+            } catch(Exception ex) {
+                MessageBox.Show("Nuk ka te dhena per te derguar.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            try {
+                if (textBox5.Text == null || textBox3.Text == null)
+                    throw new Exception();
+                textBox1.Text = textBox6.Text;
+                textBox3.Text = textBox4.Text;
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Nuk ka te dhena per te derguar.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            textBox1.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            textBox2.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
         }
     }
 
@@ -48,24 +83,40 @@ namespace CaesarCipher {
         public static string encrypt(string str, int shift) {
             string encryptedString = "";
             for(int i = 0; i < str.Length; i++)
-                if((int)str[i] >= 65 && (int)str[i] <= 90)
-                    encryptedString += (char)((int)str[i] + shift - (int)((int)(str[i] - 65 + shift) / 26) * 26);
-                else if((int)str[i] >= 97 && (int)str[i] <= 122)
-                    encryptedString += (char)((int)str[i] + shift - (int)((int)(str[i] - 97 + shift) / 26) * 26);
+                if(shift >= 0)
+                    if((int)str[i] >= 65 && (int)str[i] <= 90)
+                        encryptedString += (char)((int)str[i] + shift - (int)((int)(str[i] - 65 + shift) / 26) * 26);
+                    else if((int)str[i] >= 97 && (int)str[i] <= 122)
+                        encryptedString += (char)((int)str[i] + shift - (int)((int)(str[i] - 97 + shift) / 26) * 26);
+                    else
+                        encryptedString += str[i];
                 else
-                    encryptedString += str[i];
+                    if ((int)str[i] >= 65 && (int)str[i] <= 90)
+                        encryptedString += (char)((int)str[i] + shift + (int)((90 - (int)str[i] - shift) / 26) * 26);
+                    else if ((int)str[i] >= 97 && (int)str[i] <= 122)
+                        encryptedString += (char)((int)str[i] + shift + (int)((122 - (int)str[i] - shift) / 26) * 26);
+                    else
+                        encryptedString += str[i];
             return encryptedString;
         }
 
         public static string decrypt(string str, int shift) {
             string decryptedString = "";
             for(int i = 0; i < str.Length; i++)
-                if((int)str[i] >= 65 && (int)str[i] <= 90)
-                    decryptedString += (char)((int)str[i] - shift + (int)((90 - (int)str[i] + shift) / 26) * 26);
-                else if ((int)str[i] >= 97 && (int)str[i] <= 122)
-                    decryptedString += (char)((int)str[i] - shift + (int)((122 - (int)str[i] + shift) / 26) * 26);
+                if(shift >= 0)
+                    if((int)str[i] >= 65 && (int)str[i] <= 90)
+                        decryptedString += (char)((int)str[i] - shift + (int)((90 - (int)str[i] + shift) / 26) * 26);
+                    else if ((int)str[i] >= 97 && (int)str[i] <= 122)
+                        decryptedString += (char)((int)str[i] - shift + (int)((122 - (int)str[i] + shift) / 26) * 26);
+                    else
+                        decryptedString += str[i];
                 else
-                    decryptedString += str[i];
+                    if ((int)str[i] >= 65 && (int)str[i] <= 90)
+                        decryptedString += (char)((int)str[i] - shift - (int)((int)(str[i] - 65 - shift) / 26) * 26);
+                    else if ((int)str[i] >= 97 && (int)str[i] <= 122)
+                        decryptedString += (char)((int)str[i] - shift - (int)((int)(str[i] - 97 - shift) / 26) * 26);
+                    else
+                        decryptedString += str[i];
             return decryptedString;
         }
     }
